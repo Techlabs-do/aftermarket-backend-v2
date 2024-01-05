d20852bf8631a175c77fd078ab49bf3ee46d7aa4
shanialy <xhanialee1@gmail.com>
2024-01-05 19:03:14 +0500

    fix: changes in dtos

--- src/data/dtos/users/customer-phones.dto.ts
+++ src/data/dtos/users/customer-phones.dto.ts
@@ -1,6 +1,14 @@
-import { IsNotEmpty } from 'class-validator';
+import { Type } from 'class-transformer';
+import { IsArray, IsNotEmpty, ValidateNested } from 'class-validator';
+export class CustomerPhoneDto {
+  @IsNotEmpty()
+  readonly number: string;
 
-export class CustomerPhonesDto {
+  @IsNotEmpty()
+  readonly contact_person: string;
+}
+
+export class CreateFrameworksDto {
   @IsNotEmpty()
   readonly number: string;
 
@@ -10,12 +18,10 @@ export class CustomerPhonesDto {
   @IsNotEmpty()
   readonly user_id: number;
 }
-[];
 
-export class CustomerPhoneDto {
-  @IsNotEmpty()
-  readonly number: string;
-
-  @IsNotEmpty()
-  readonly contact_person: string;
+export class CustomerPhonesDto {
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => CreateFrameworksDto)
+  readonly data: CreateFrameworksDto[];
 }
--- src/data/dtos/users/vendor-phones.dto.ts
+++ src/data/dtos/users/vendor-phones.dto.ts
@@ -1,6 +1,15 @@
-import { IsNotEmpty } from 'class-validator';
+import { Type } from 'class-transformer';
+import { IsArray, IsNotEmpty, ValidateNested } from 'class-validator';
 
-export class VendorPhonesDto {
+export class VendorPhoneDto {
+  @IsNotEmpty()
+  readonly number: string;
+
+  @IsNotEmpty()
+  readonly contact_person: string;
+}
+
+export class CreateFrameworksDto {
   @IsNotEmpty()
   readonly number: string;
 
@@ -10,12 +19,10 @@ export class VendorPhonesDto {
   @IsNotEmpty()
   readonly user_id: number;
 }
-[];
 
-export class VendorPhoneDto {
-  @IsNotEmpty()
-  readonly number: string;
-
-  @IsNotEmpty()
-  readonly contact_person: string;
+export class VendorPhonesDto {
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => CreateFrameworksDto)
+  readonly data: CreateFrameworksDto[];
 }
